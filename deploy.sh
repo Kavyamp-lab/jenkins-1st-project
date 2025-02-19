5ef83696a71edcae664d4cd2947daa4a6bc5e027
Kavyamp-lab <kavyampatagar@gmail.com>
2025-02-19 19:56:10 +0530

    Updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 # deploy.sh
 echo "Deploying to simulated production..."
 # Replace /path/to/web/server/directory with the actual path
-cp -r * /path/to/web/server/directory  # * copies all files and directories
+cp -r * aws s3 cp . s3://jenkins-projwct-bucket  --recursive --acl public-read  # * copies all files and directories
 
 echo "Deployment complete."
